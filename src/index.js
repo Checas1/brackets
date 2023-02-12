260c08281150a5a5359784e574a660282ec77a95
checas1 <checas104@gmail.com>
2023-02-12 13:45:52 +0200

    init: complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = [...str]
+    console.log(array)
+    if (str.length % 2 != 0) return false
+    for(let i = 0; i < array.length; i++) {
+      for (let item of bracketsConfig){
+        if(array[i] == item[0] && array[i+1] == item[1]){
+          array.splice(i, 2)
+          i = -1
+        }
+      }
+    }
+    return (array.length == 0) ? true : false
 }
